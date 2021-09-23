248700b51232a758aa8241a14fc49b5af1a0718d
chengjianwen <chengjianwen@gmail.com>
2021-09-23 22:19:59 +0800

    根据电路板做了修改

--- source/piguard.cpp
+++ source/piguard.cpp
@@ -2,23 +2,32 @@
   * 文件名     : piguard.cpp
   * 作者       : pi-classroom.com
   * 版本       : 0.1
-  * 说明       : 通过P0-P2控制L298N的ENA、IN1和IN2，实现门锁的开锁和关锁
+  * 说明       : 通过P3-P7控制电机驱动版，以及一个两路继电器。实现门锁控制和LED屏的控制
   *                LU1000              L298N
-  *                  P0    <-------->   ENA
-  *                  P1    <-------->   IN1
+  *                  P3    <-------->   K1
+  *                  P4    <-------->   IN1
   *                  P2    <-------->   IN2
   *              程序需要通过pilock或者piunlock的命令来执行
   *****************************************************************************/
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include "usb2gpio.h"
 #include "usb_device.h"
 
+#define		P3	(1 << 3)	// K2
+#define		P4	(1 << 4)	// K1
+#define		P5	(1 << 5)	// noused
+#define		P6	(1 << 6)	// motor-0
+#define		P7	(1 << 7)	// motor-1
+
 int main(int argc, const char* argv[])
 {
     int  handle;
-    int  lock = -1;
+    int  lock = -1;	// lock/unlock
+    int  on = -1;	// on/off
+    int  index;		// index of on/off
 
     char *p;
     p = (char *)argv[0] + strlen(argv[0]);
@@ -29,10 +38,28 @@ int main(int argc, const char* argv[])
         lock = 1;
     else if (strcmp(p, "piunlock") == 0)
         lock = 0;
+    else {
+      if (strcmp(p, "pion") == 0)
+        on = 1;
+      else if (strcmp(p, "pioff") == 0)
+        on = 0;
 
-    if (lock < 0){
-        printf("Neigher lock nor unlock!\n");
-        return 0;
+      if (argc != 2)
+      {
+        fprintf (stderr, "Usage %s <index>\n", argv[0]);
+        return -1;
+      }
+      index = atoi(argv[1]);
+      if (index != 1 &&  index != 2)
+      {
+        fprintf (stderr, "index should be 1 or 2\n");
+        return -1;
+      }
+    }
+
+    if (lock < 0 && on < 0){
+        fprintf(stderr, "couldn't resolv the command!\n");
+        return -1;
     }
 
     //扫描查找设备
@@ -68,42 +95,64 @@ int main(int argc, const char* argv[])
 
     //设置输出模式――没上下拉电阻
     ret = GPIO_SetOutput(handle, 
-                         0xF,		// Pin Mask, 0-15bit
-                         0		// 0: 无上拉和下拉电阻
-                         		// 1: 使能上拉电阻
-                         		// 2: 使能下拉电阻
+                         P3 | P4 | P5 | P6 | P7,	// Pin Mask, 0-2bit
+                         0				// 0: 无上拉和下拉电阻
+                         				// 1: 使能上拉电阻
+                         				// 2: 使能下拉电阻
                          );
     if (ret != GPIO_SUCCESS){
         printf("error on GPIO_SetOutput(): %d!\n", ret);
         return -6;
     }
 
-    // 控制电机正转
-    // P0输出高电平，使能L298N的A通道
-    // P1输出低电平，P2输出高电平，正转
-    // 输出信号为: 1 << 0 | 0 << 1 | 1 << 2
-    ret = GPIO_Write(handle,			// 设备句柄
-                     0x7,			// Pin Mask，0-2bit
-                     lock ? 1<< 0 | 0 << 1 | 1 << 2 : 1 << 0 | 1 << 1 | 0 << 2	// Pin Value，lock时0-2bit分别为1-0-1，unlock时0-2bit位分别为1-1-0
-                     );
-    if (ret != GPIO_SUCCESS){
-        printf("error on GPIO_Write(): %d!\n", ret);
-        return -7;
-    }
-    usleep(500000);	// 延时0.5秒，即500000微秒
+    if (lock >= 0) {
+      // P6输出高电平，P7输出低电平为正转，反之为反转
+      ret = GPIO_Write(handle,		// 设备句柄
+                       P6 | P7,		// Pin Mask
+                       lock ? P6 : P7);	// Pin Value
+      if (ret != GPIO_SUCCESS){
+          fprintf(stderr, "error on GPIO_Write(): %d!\n", ret);
+          return -1;
+      }
 
-    // 电机停止
-    // P0输出低电平，使能L298N的A通道
-    // 输出信号为: 0 << 0
-    ret = GPIO_Write(handle,	// 设备句柄
-                     0x1,	// Pin Mask，0bit
-                     0 << 0 	// Pin Value，0bit为0
-                     );
-    if (ret != GPIO_SUCCESS){
-        printf("error on GPIO_Write(): %d!\n", ret);
-        return -7;
-    }
+      // 等待1秒钟
+      sleep(1);
 
+      // 停止电机
+      ret = GPIO_Write(handle,		// 设备句柄
+                     P6 | P7,		// Pin Mask
+                     0); 		// Pin Value
+      if (ret != GPIO_SUCCESS){
+          fprintf(stderr, "error on GPIO_Write(): %d!\n", ret);
+          return -1;
+      }
+    } else if (on == 0) {
+      if (index == 1)
+        ret = GPIO_Write(handle,		// 设备句柄
+                         P4,
+                         0);
+      else if (index == 2)
+        ret = GPIO_Write(handle,		// 设备句柄
+                         P3,
+                         0);
+      if (ret != GPIO_SUCCESS) {
+        fprintf(stderr, "error on GPIO_Write(): %d!\n", ret);
+        return -1;
+      }
+    } else if (on == 1) {
+      if (index == 1)
+        ret = GPIO_Write(handle,		// 设备句柄
+                         P4,
+                         P4);
+      else if (index == 2)
+        ret = GPIO_Write(handle,		// 设备句柄
+                         P3,
+                         P3);
+      if (ret != GPIO_SUCCESS) {
+        fprintf(stderr, "error on GPIO_Write(): %d!\n", ret);
+        return -1;
+      }
+    }
     //关闭设备
     ret = USB_CloseDevice(DevHandle[0]);
     if(!ret){
